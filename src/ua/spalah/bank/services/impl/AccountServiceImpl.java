03de945e8f1f98cc3c145f3ec46c9ccfc072b459
Dmitry Dev <dmitry.dev@gmail.com>
2017-01-11 21:28:09 +0200

    fix withdraw

--- src/ua/spalah/bank/services/impl/AccountServiceImpl.java
+++ src/ua/spalah/bank/services/impl/AccountServiceImpl.java
@@ -1,5 +1,6 @@
 package ua.spalah.bank.services.impl;
 
+import ua.spalah.bank.exceptions.OverdraftLimitExceededException;
 import ua.spalah.bank.services.Account;
 import ua.spalah.bank.model.enums.AccountType;
 import ua.spalah.bank.exceptions.NotEnoughFundsException;
@@ -13,39 +14,43 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void deposit(Account account, double amount) {
 
-            if (amount < 0) {
-                throw new IllegalArgumentException();
-            } else {
-                account.setBalance(account.getBalance() + amount);
-            }
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            account.setBalance(account.getBalance() + amount);
+        }
 
     }
 
     @Override
-    public void withdraw(Account account, double amount)throws NotEnoughFundsException { // TODO: 11.01.2017 Fix this
-            if (amount < 0) {
-                throw new IllegalArgumentException();
-            } else {
-                AccountType accountType = account.getType();
-                if (accountType == AccountType.CHECKING) {
-                    account = (CheckingAccount) account;
-                    if ((account.getBalance() + ((CheckingAccount) account).getOverdraft()) >= amount) {
-                        account.setBalance(account.getBalance() - amount);
-                    } else {
-                        throw new NotEnoughFundsException("Not Enough Funds");
-                    }
-                } else if (accountType == AccountType.SAVING) {
-                    if (account.getBalance() >= amount) {
-                        account.setBalance(account.getBalance() - amount);
-                    }
+    public void withdraw(Account account, double amount) throws NotEnoughFundsException{
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            AccountType accountType = account.getType();
+            if (accountType == AccountType.CHECKING) {
+                if ((account.getBalance() + ((CheckingAccount) account).getOverdraft()) >= amount) {
+                    account.setBalance(account.getBalance() - amount);
+                } else {
+                    throw new OverdraftLimitExceededException("Overdraft limit exceeded");
                 }
+            } else if (accountType == AccountType.SAVING) {
+                if (account.getBalance() >= amount) {
+                    account.setBalance(account.getBalance() - amount);
+                } else {
+                    throw new NotEnoughFundsException("Not Enough Funds");
+                }
+            } else {
+                throw new IllegalArgumentException("Unknown account type");
             }
+        }
 
     }
+
     @Override
     public void transfer(Account fromAccount, Account toAccount, double amount) throws NotEnoughFundsException {
-               withdraw(fromAccount, amount);
-               deposit(toAccount, amount);
-        }
+        withdraw(fromAccount, amount);
+        deposit(toAccount, amount);
+    }
 
 }
